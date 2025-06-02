06da2b2c5516510d0d0db7510915f032d773c837
JPedroo <joaopedrojp21@outlook.com>
2025-06-02 01:05:20 -0300

    carousel

--- src/scripts/animation.js
+++ src/scripts/animation.js
@@ -1,20 +1,3 @@
-function animateClockElement(element, newValue) {
-  const el = document.getElementById(element);
-  el.style.transition = 'transform 0.5s ease-in-out';
-  el.style.transform = 'scale(0.9)';
-  
-  setTimeout(() => {
-    el.textContent = newValue;
-    el.style.transform = 'scale(1)';
-  }, 250);
-}
-
-function updateClockAnimation(hours, minutes, seconds) {
-  animateClockElement('hours', hours);
-  animateClockElement('minutes', minutes);
-  animateClockElement('seconds', seconds);
-}
-
 /*--------------------
 Vars
 --------------------*/
